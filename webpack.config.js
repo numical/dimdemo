6688c9912b6e1fc2f0b45bf144249a45bad6a904
Mike Evans <mike.evans@numical.com>
2019-09-22 12:19:20 +0100

    not bundling node_modules

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,25 @@
 const webpack = require("webpack");
+var path = require('path');
+var fs = require('fs');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+// thanks: https://jlongster.com/Backend-Apps-with-Webpack--Part-I
+const nodeModules = fs.readdirSync('node_modules')
+  .filter(x => ['.bin'].indexOf(x) === -1)
+  .reduce((nodeModules, mod) => {
+    nodeModules[mod] = 'commonjs ' + mod;
+    return nodeModules
+  }, {});
 
 module.exports = {
   entry: "./src/server.js",
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: "babel-loader"
-          }
-        ]
-      }
-    ]
-  },
-  resolve: {
-    extensions: ["*", ".js"]
-  },
+  target: "node",
+  externals: nodeModules,
   output: {
-    path: __dirname + "/public",
+    path: __dirname + "/build",
     publicPath: "/",
     filename: "alphabet-server.js"
   },
-  target: "node",
   plugins: [
     new webpack.EnvironmentPlugin(
       'DIM_SWITCH_A',
